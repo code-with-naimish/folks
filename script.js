6fe0d25d4f4f7dcb7490c7c0aa8969d8d3bf4db9
code-with-naimish <naimish028@gmail.com>
2024-11-10 20:50:47 +0530

    ui fix

---
+++ script.js
@@ -0,0 +1,25 @@
+// "use strict";
+
+// // const fetchUser = async function () {
+// //   const loaderBox = document.getElementById("loader-box");
+// //   const noDataBox = document.getElementById("no-data-box");
+// //   const mainData = document.getElementById("main-data");
+// //   noDataBox.classList.add("hidden");
+// //   mainData.classList.add("hidden");
+
+// //   try {
+// //     const response = await fetch("https://dummyjson.com/users");
+// //     if (!response.ok) {
+// //       throw new Error("Something went wrong! Please try again later ");
+// //     }
+
+// //     const data = await response.json();
+// //   } catch (error) {
+// //     console.log(error);
+// //     noDataBox.classList.remove("hidden");
+// //   } finally {
+// //     loaderBox.classList.add("hidden");
+// //   }
+// // };
+
+// // fetchUser();
